7ccb2f7b6c8f38fd92652dda41433b78d69d8277
kamiazya <yuki@kamiazya.tech>
2021-09-01 22:07:40 +0900

    use poetry and use dev-dependencies

--- api/app/main.py
+++ api/app/main.py
@@ -1,13 +1,15 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
+from fastapi import Request
 
 app = FastAPI()
 
-@app.post('/')
+
+@app.post("/")
 async def upload(request: Request):
     chunk_sizes = [len(chunk) async for chunk in request.stream()][:-1]
     return {
-        'chunkSizes': chunk_sizes,
-        'totalSize': sum(chunk_sizes),
-        'maxSize': max(chunk_sizes),
-        'minSize': min(chunk_sizes),
+        "chunkSizes": chunk_sizes,
+        "totalSize": sum(chunk_sizes),
+        "maxSize": max(chunk_sizes),
+        "minSize": min(chunk_sizes),
     }
